1e39b2d1fa99d317fc59069e3559ce1dec1b68e4
Ishan Udawatte <ishan.20232686@iit.ac.lk>
2026-06-02 20:12:19 +0530

    Developing hero section animation 50:18

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,4 +1,6 @@
 import {Button} from "@/components/Button";
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
 
 
 const navLinks = [
@@ -10,8 +12,9 @@ const navLinks = [
 ]
 
 export const Navbar = () => {
+    const [isMobileMenuOpen , setIsMobileMenuOpen] = useState(false);
     return (
-        <header className="fixed top-0 left-0 bg-transparent py-5">
+        <header className="fixed top-0 left-0 bg-transparent py-5 z-50">
             <nav className="container mx-auto flex items-center justify-between">
                 <a href='#' 
                 className="text-xl font-bold tracking-tight hover:text-primary">
@@ -22,8 +25,12 @@ export const Navbar = () => {
                 <div className="hidden md:flex items-center gap-1">
                     <div className="glass rounded-full px-2 py-1 flex items-center gap-1">
                         {navLinks.map((link, index) => (
-                            <a href={link.href}key={index} className="px-4 py-2 text-sm text-muted-foreground hover:text-foreground round-full hover:bg-surface">
-                                {link.label}</a>
+                            <a 
+                            href={link.href}
+                            key={index} 
+                            className="px-4 py-2 text-sm text-muted-foreground hover:text-foreground round-full hover:bg-surface">
+                                {link.label}
+                            </a>
                         ))};
                     </div>
 
@@ -34,7 +41,29 @@ export const Navbar = () => {
                 </div>
 
                 {/* Mobile Menu Button */}
-                <button className="md:hidden p-2 text-foreground"></button>
+                <button 
+                className="md:hidden p-2 text-foreground cursor-pointer" 
+                onClick={()=> setIsMobileMenuOpen((prev) => !prev)}>
+                    {isMobileMenuOpen ? <X size={24} /> : <Menu size={24} />}
+                </button>
             </nav>
+
+            {/* Mobile Menu */}
+            {isMobileMenuOpen && (
+                <div className="animate-fade-in md:hidden glass-strong" >
+                <div className="container glass-strong mx-auto px-6 py-6 flex flex-col gap-4">
+                    {navLinks.map((link, index) => (
+                            <a 
+                            href={link.href}
+                            key={index} 
+                            className="text-lg text-muted-foreground hover:text-foreground py-2">
+                                {link.label}
+                            </a>
+                        ))}
+                        <Button size="sm">Contact Me</Button>
+                </div>
+            </div>
+
+            )}
         </header>);
 };
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,3 +1,28 @@
 export const Hero = () => {
-    return <section></section>
+    return <section className="relative min-h-screen flex items-center overflow-hidden">
+        {/* background */}
+
+        <div className="absolute inset-0">
+            <img 
+            src="/assets/hero.png"
+            alt="Hero img"
+            className="w-full h-full object-cover opacity-40"/>
+            <div className="absolute inset-0 bg-gradient-to-b from-background/20 via-backgrounf/80 to-background"/>
+        </div>
+
+        {/* Green Dots */}
+        <div className="absolute inset-0 overflow-hidden pointer-events-none">
+            {[...Array(30)].map((_, i) => (
+                <div 
+                className="absolute w-1.5 h-1.5 rounded-full opacity-60"
+                style={{
+                    backgrounfColor: "#20B2A6",
+                    left: '${Math.random() * 100}%',
+                    top: '${Math.random() * 100}%',
+                    animation: 'slow-drift 50s'
+                }}
+                />
+            ))}
+        </div>
+    </section>
 }
